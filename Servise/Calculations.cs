b51166ea60779af010feaff7701ce20523521ffe
Shalom Raichman <shalomr1770@gmail.com>
2024-08-22 14:45:20 +0300

    som progres

---
+++ Servise/Calculations.cs
@@ -0,0 +1,20 @@
+﻿using MossadAgentsAPI.Models;
+
+namespace MossadAgentsAPI.Servise
+{
+    public static class Calculations
+    {
+        public static double GetDistans(Coordinates agentCoordinates, Coordinates targetCoordinates)
+        {
+            double x1 = agentCoordinates.X;
+            double y1 = agentCoordinates.Y;
+            double x2 = targetCoordinates.X;
+            double y2 = targetCoordinates.Y;
+
+            double result =  Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+
+            return result;
+        }
+    }
+}
---
+++ Servise/StaffRecommendation.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MossadAgentsAPI.Data;
+using MossadAgentsAPI.Models;
+using System.Diagnostics.Eventing.Reader;
+
+namespace MossadAgentsAPI.Servise
+{
+    public static class StaffRecommendation
+    {
+        private static readonly MossadAgentsAPIContext _context;
+
+
+        private static List<Agent> _agents;
+        private static Agent _relevantAgent;
+        private static Target _relevantTarget;
+
+        // bool func return if agent is < 200 (targetlocation)
+        // distans <= 200
+
+        public static void IsInRange(/*Coordinates targetCoordinates*/)
+        {
+            //foreach (var agent in _context.Agents)
+            //{
+
+            //}
+        }
+
+        //public static Mission GetMissionMatch(Mission mission)
+        //{
+        //    double minDistance = int.MinValue;
+
+        //    foreach (var target in _context.Targets)
+        //    {
+        //        foreach (var agent in _context.Agents)
+        //        {
+        //            if (agent != null && target != null)
+        //            {
+        //                double distans = Calculations.GetDistans(agent.coordinates, target.coordinates);
+        //                if (distans < minDistance) { minDistance = distans; }
+
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
